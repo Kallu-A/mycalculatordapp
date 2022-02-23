55d407d233def71eb0c474c47e77047db32f2c6d
Kallu-A <lucas.aries@protonmail.com>
2022-02-23 22:32:46 +0100

    Init Commit

--- programs/mycalculatordapp/src/lib.rs
+++ programs/mycalculatordapp/src/lib.rs
@@ -19,25 +19,7 @@ pub struct Create<'info> {
 }
 
 #[derive(Accounts)]
-pub struct Addition<'info> {
-    #[account(mut)]
-    pub calculator: Account<'info, Calculator>,
-}
-
-#[derive(Accounts)]
-pub struct Multiplication<'info> {
-    #[account(mut)]
-    pub calculator: Account<'info, Calculator>,
-}
-
-#[derive(Accounts)]
-pub struct Subtraction<'info> {
-    #[account(mut)]
-    pub calculator: Account<'info, Calculator>,
-}
-
-#[derive(Accounts)]
-pub struct Division<'info> {
+pub struct Calculate<'info> {
     #[account(mut)]
     pub calculator: Account<'info, Calculator>,
 }
@@ -52,25 +34,25 @@ pub mod mycalculatordapp {
         Ok(())
     }
 
-    pub fn add(ctx: Context<Addition>, num1: i64, num2: i64) -> ProgramResult {
+    pub fn add(ctx: Context<Calculate>, num1: i64, num2: i64) -> ProgramResult {
         let calculator = &mut ctx.accounts.calculator;
         calculator.result = num1 + num2;
         Ok(())
     }
 
-    pub fn multiply(ctx: Context<Multiplication>, num1: i64, num2: i64) -> ProgramResult {
+    pub fn multiply(ctx: Context<Calculate>, num1: i64, num2: i64) -> ProgramResult {
         let calculator = &mut ctx.accounts.calculator;
         calculator.result = num1 * num2;
         Ok(())
     }
 
-    pub fn subtract(ctx: Context<Subtraction>, num1: i64, num2: i64) -> ProgramResult {
+    pub fn subtract(ctx: Context<Calculate>, num1: i64, num2: i64) -> ProgramResult {
         let calculator = &mut ctx.accounts.calculator;
         calculator.result = num1 - num2;
         Ok(())
     }
 
-    pub fn divide(ctx: Context<Division>, num1: i64, num2: i64) -> ProgramResult {
+    pub fn divide(ctx: Context<Calculate>, num1: i64, num2: i64) -> ProgramResult {
         let calculator = &mut ctx.accounts.calculator;
         calculator.result = num1 / num2;
         calculator.remainder = num1 % num2;
